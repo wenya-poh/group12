b503a79a9203ca2a57617991a32003a486e22656
dlcgx <43769810+dlcgx@users.noreply.github.com>
2019-04-02 18:40:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ file_titles = []
 for file in dirs:
    file_titles.append(file)
 
+for i in range(len(file_titles)):
+    file_titles[i] = file_titles[i].replace(".txt","")
+    
+
+#extract id, artist, title
+list_1 = []
+    
+def extract_(x):
+    
+    import re
+    for i in range(1000):
+        b = re.split('~' ,x[i])   
+        dict_1 = {"id" : b[0], "artist" : b[1], "title" : b[2], 'kid_safe': 0, 'love': 0, 'mood': 0, 'length': 0, 'complexity': 0}
+        list_1.append(dict_1)
+    return list_1
+    
+extract_(file_titles)
 
 
 textfile = open("lyrics/000~Jerry-Harrison~No-More-Reruns.txt")
