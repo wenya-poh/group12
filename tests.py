ac3c511ec70055b0ae308808427755c67f7bf268
dlcgx <43769810+dlcgx@users.noreply.github.com>
2019-05-06 23:20:32 -0400

    upload test file

--- tests.py
+++ tests.py
@@ -35,24 +35,24 @@ song_ID_sorted = main.song_ID_list(file_titles)
 song_ID_sorted.sort()
 lyric_dict = {}
 for n in [0,156,641,541,961,1000]:
-    lyric_dict[song_ID_sorted[n]] = read_song(path+'/'+file_txt_names[n])
+    lyric_dict[song_ID_sorted[n]] = main.read_song(path+'/'+file_txt_names[n])
     
 song_ID_test = ['000','155','540','640','960','999']   
 
 
 #Test read_song
-read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")
+main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")
 
 class TestRead_Song(unittest.TestCase):
 
     def test_type(self): 
-        self.assertEqual(type(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")), list)
+        self.assertEqual(type(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")), list)
 
     def test_length(self):
-        self.assertEqual(len(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),369)
+        self.assertEqual(len(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),369)
         
     def test_lower(self):
-        for i in read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt"):
+        for i in main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt"):
             self.assertEqual(i, i.lower())
 
 
@@ -61,20 +61,19 @@ suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestRead_Song)
 # Run each test in suite
 unittest.TextTestRunner().run(suite)
 
-type(song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","001~Dead-Kennedys~Police-Truck"]))
 
 #test song_ID_list
 
 class TestSong_ID(unittest.TestCase):
 
     def test_type(self): 
-        self.assertEqual(type(song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","Lyrics/001~Dead-Kennedys~Police-Truck"])), list)
+        self.assertEqual(type(main.song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","Lyrics/001~Dead-Kennedys~Police-Truck"])), list)
 
     def test_song1(self):
-        self.assertEqual(song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","Lyrics/001~Dead-Kennedys~Police-Truck"])[0],'000')
+        self.assertEqual(main.song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","Lyrics/001~Dead-Kennedys~Police-Truck"])[0],'000')
 
     def test_song2(self):
-        self.assertEqual(song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","Lyrics/001~Dead-Kennedys~Police-Truck"])[1],'001')    
+        self.assertEqual(main.song_ID_list(["Lyrics/000~Jerry-Harrison~No-More-Reruns.txt","Lyrics/001~Dead-Kennedys~Police-Truck"])[1],'001')    
         
 
 # Get tests as a test suite
@@ -88,13 +87,13 @@ unittest.TextTestRunner().run(suite)
 class TestSong_lines(unittest.TestCase):
 
     def test_type(self): 
-        self.assertEqual(type(read_song_lines(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")), list)
+        self.assertEqual(type(main.read_song_lines(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")), list)
 
     def test_length(self):
-        self.assertEqual(len(read_song_lines(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),71)
+        self.assertEqual(len(main.read_song_lines(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),71)
         
     def test_lower(self):
-        for i in read_song_lines(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt"):
+        for i in main.read_song_lines(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt"):
             self.assertEqual(i, i.lower())
 
 
@@ -111,10 +110,10 @@ unittest.TextTestRunner().run(suite)
 class TestScaler(unittest.TestCase):
 
     def test_even(self): 
-        self.assertEqual(scaler(100,0,50),0.5)
+        self.assertEqual(main.scaler(100,0,50),0.5)
 
     def test_odd(self):
-        self.assertEqual(scaler(3,1,2), 0.5)
+        self.assertEqual(main.scaler(3,1,2), 0.5)
         
 
 # Get tests as a test suite
@@ -128,7 +127,7 @@ unittest.TextTestRunner().run(suite)
 class TestSong_length(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(detect_length_count(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),369)
+        self.assertEqual(main.detect_length_count(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),369)
         
 
 # Get tests as a test suite
@@ -142,7 +141,7 @@ unittest.TextTestRunner().run(suite)
 class TestMax_Min_length(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(max_min_length(lyric_dict),(369,31))
+        self.assertEqual(main.max_min_length(lyric_dict),(369,31))
         
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestMax_Min_length)
@@ -152,20 +151,17 @@ unittest.TextTestRunner().run(suite)
 
 #test length output
 
-len(length(lyric_dict))
-
-
 
 class Testlength(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(len(length(lyric_dict)),6)
+        self.assertEqual(len(main.length(lyric_dict)),6)
         
     def test_1(self):
-        self.assertEqual(length(lyric_dict)['000'],1.0)
+        self.assertEqual(main.length(lyric_dict)['000'],1.0)
 
     def test_2(self):
-        self.assertEqual(length(lyric_dict)['999'],0)        
+        self.assertEqual(main.length(lyric_dict)['999'],0)        
        
         
 # Get tests as a test suite
@@ -175,15 +171,14 @@ unittest.TextTestRunner().run(suite)
 
 #test kid safe
 
-detect_profane_count(read_song(r"Lyrics/540~The-Exploited~Beat-the-Bastards.txt"))
 
 class Testdetectprofane(unittest.TestCase):
 
     def test_1(self):
-        self.assertEqual(detect_profane_count(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),0)
+        self.assertEqual(main.detect_profane_count(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),0)
         
     def test_2(self):
-        self.assertEqual(detect_profane_count(read_song(r"Lyrics/540~The-Exploited~Beat-the-Bastards.txt")),2)
+        self.assertEqual(main.detect_profane_count(main.read_song(r"Lyrics/540~The-Exploited~Beat-the-Bastards.txt")),2)
             
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testdetectprofane)
@@ -192,12 +187,11 @@ unittest.TextTestRunner().run(suite)
 
 #Test maxmin profanity
 
-max_min_profanity(lyric_dict)
 
 class TestMax_Min_length(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(max_min_profanity(lyric_dict),(2,0))
+        self.assertEqual(main.max_min_profanity(lyric_dict),(2,0))
         
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestMax_Min_length)
@@ -206,19 +200,16 @@ unittest.TextTestRunner().run(suite)
 
 
 #test profanity output
-
-kid(lyric_dict)
-
 class Testkid(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(len(kid(lyric_dict)),6)
+        self.assertEqual(len(main.kid(lyric_dict)),6)
         
     def test_1(self):
-        self.assertEqual(kid(lyric_dict)['000'],1.0)
+        self.assertEqual(main.kid(lyric_dict)['000'],1.0)
 
     def test_2(self):
-        self.assertEqual(kid(lyric_dict)['540'],0)        
+        self.assertEqual(main.kid(lyric_dict)['540'],0)        
        
         
 # Get tests as a test suite
@@ -227,14 +218,13 @@ suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testkid)
 unittest.TextTestRunner().run(suite)       
 
 #test complexity
-detect_complexity_count(lyric_dict['155'])
 
 class Testdetect_complexity_count(unittest.TestCase):
 
     def test_1(self):
-        self.assertEqual(detect_complexity_count(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),188)
+        self.assertEqual(main.detect_complexity_count(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),188)
     def test_2(self):
-        self.assertEqual(detect_complexity_count(lyric_dict['155']),208)
+        self.assertEqual(main.detect_complexity_count(lyric_dict['155']),208)
 
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testdetect_complexity_count)
@@ -243,12 +233,10 @@ unittest.TextTestRunner().run(suite)
 
 #Test maxmincomplex 
 
-max_min_complexity(lyric_dict)
-
 class TestMax_Min_complexity(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(max_min_complexity(lyric_dict),(208,22))
+        self.assertEqual(main.max_min_complexity(lyric_dict),(208,22))
         
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestMax_Min_complexity)
@@ -258,20 +246,16 @@ unittest.TextTestRunner().run(suite)
 
 #test complex output
 
-complexity(lyric_dict)
-
-
-
 class Testcomplex(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(len(complexity(lyric_dict)),6)
+        self.assertEqual(len(main.complexity(lyric_dict)),6)
         
     def test_1(self):
-        self.assertEqual(complexity(lyric_dict)['155'],1.0)
+        self.assertEqual(main.complexity(lyric_dict)['155'],1.0)
 
     def test_2(self):
-        self.assertEqual(complexity(lyric_dict)['999'],0)        
+        self.assertEqual(main.complexity(lyric_dict)['999'],0)        
        
         
 # Get tests as a test suite
@@ -281,15 +265,12 @@ unittest.TextTestRunner().run(suite)
 
 
 #TEST MOOD
-
-detect_song_mood(lyric_dict['000'])
-
 class Testdetect_songmood(unittest.TestCase):
 
     def test_1(self):
-        self.assertEqual(detect_song_mood(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),0.5073768658321502)
+        self.assertEqual(main.detect_song_mood(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),0.5073768658321502)
     def test_2(self):
-        self.assertEqual(detect_song_mood(lyric_dict['155']),0.5)
+        self.assertEqual(main.detect_song_mood(lyric_dict['155']),0.5)
 
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testdetect_songmood)
@@ -302,7 +283,7 @@ unittest.TextTestRunner().run(suite)
 class TestMax_Min_mood(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(max_min_mood(lyric_dict),(0.525925925925926, 0.49231340763255654))
+        self.assertEqual(main.max_min_mood(lyric_dict),(0.525925925925926, 0.49231340763255654))
         
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestMax_Min_mood)
@@ -316,13 +297,13 @@ unittest.TextTestRunner().run(suite)
 class Testmood(unittest.TestCase):
 
     def test_length(self): 
-        self.assertEqual(len(mood(lyric_dict)),6)
+        self.assertEqual(len(main.mood(lyric_dict)),6)
         
     def test_1(self):
-        self.assertEqual(mood(lyric_dict)['960'],1.0)
+        self.assertEqual(main.mood(lyric_dict)['960'],1.0)
 
     def test_2(self):
-        self.assertEqual(mood(lyric_dict)['540'],0)        
+        self.assertEqual(main.mood(lyric_dict)['540'],0)        
        
         
 # Get tests as a test suite
@@ -333,14 +314,12 @@ unittest.TextTestRunner().run(suite)
 
 #TEST LOVE
 
-detect_love_count(lyric_dict['000'])
-
 class Testdetect_love_count(unittest.TestCase):
 
     def test_1(self):
-        self.assertEqual(detect_love_count(read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),60)
+        self.assertEqual(main.detect_love_count(main.read_song(r"Lyrics/000~Jerry-Harrison~No-More-Reruns.txt")),60)
     def test_2(self):
-        self.assertEqual(detect_love_count(lyric_dict['155']),9)
+        self.assertEqual(main.detect_love_count(lyric_dict['155']),9)
 
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testdetect_love_count)
@@ -349,12 +328,10 @@ unittest.TextTestRunner().run(suite)
 
 #Test maxmin love
 
-max_min_love(lyric_dict)
-
 class TestMax_Min_love(unittest.TestCase):
 
     def test_maxmin(self):
-        self.assertEqual(max_min_love(lyric_dict),(60,0))
+        self.assertEqual(main.max_min_love(lyric_dict),(60,0))
         
 # Get tests as a test suite
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestMax_Min_love)
@@ -367,13 +344,13 @@ unittest.TextTestRunner().run(suite)
 class Testlove(unittest.TestCase):
 
     def test_length(self):
-        self.assertEqual(len(complexity(lyric_dict)),6)
+        self.assertEqual(len(main.love(lyric_dict)),6)
         
     def test_1(self):
-        self.assertEqual(love(lyric_dict)['000'],1.0)
+        self.assertEqual(main.love(lyric_dict)['000'],1.0)
 
     def test_2(self):
-        self.assertEqual(love(lyric_dict)['999'],0)        
+        self.assertEqual(main.love(lyric_dict)['999'],0)        
        
         
 # Get tests as a test suite
@@ -381,7 +358,51 @@ suite = unittest.defaultTestLoader.loadTestsFromTestCase(Testlove)
 # Run each test in suite
 unittest.TextTestRunner().run(suite)     
 
+#json1_data = json.loads(main.main('Lyrics'))["characterizations"]
+#
+#json.loads(main.main('Lyrics'))["characterizations"][1000]["id"]
+# TEST FINAL OUTPUT
+class TestOutput(unittest.TestCase):
 
+    
+    
+    def test_json(self):
+        self.assertEqual(type(main.main('Lyrics')),str)
+        
+    def test_length(self):
+        self.assertEqual(len(json.loads(main.main('Lyrics'))["characterizations"]),1001)
+
+    def test_type_dict(self):
+        self.assertEqual(type(json.loads(main.main('Lyrics'))["characterizations"][0]),dict)        
+       
+    def test_song_999_id(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][1000]["id"], "999")        
+
+    def test_song_112_artist(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][113]["artist"], "Sub Focus")
+
+    def test_song_256_title(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][257]["title"], "Ai Mouraria")
+
+    def test_song_413_kid(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][414]["kid_safe"], 1.0)
+        
+    def test_song_574_love(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][575]["love"], 0.11111111111111105)
+    
+    def test_song_664_mood(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][665]["mood"], 0.36310853059401493)
+
+    def test_song_794_length(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][795]["love"], 0.2222222222222222)
+  
+    def test_song_934_complex(self):
+        self.assertEqual(json.loads(main.main('Lyrics'))["characterizations"][935]["complexity"], 0.34463276836158196)
+  
+# Get tests as a test suite
+suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestOutput)
+# Run each test in suite
+unittest.TextTestRunner().run(suite)     
 
         
         
